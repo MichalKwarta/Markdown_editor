ab115556aaa88885b9c4a0c4d4fc88d802aeb20d
MichalKwarta <Michu1164@gmail.com>
2022-04-25 10:59:05 +0200

    change root dir of git repo

--- src/helpers/helperfunctions.tsx
+++
@@ -1,8 +0,0 @@
-export const sliceHelper = (str:string, ...slices:number[][]) => { 
-    
-    let newString = ""
-    for (const sliceElem of slices) { 
-        newString+=str.slice(...sliceElem)
-    }
-    return newString
-}
